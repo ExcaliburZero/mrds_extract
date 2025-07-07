2d7df556cbf8ee3063cd361d4a7728ec1486e525
Christopher Wells <cwellsny@nycap.rr.com>
2025-07-06 18:24:16 -0700

    Add note about save file format

--- mrds_util/raw/_save_file.py
+++ mrds_util/raw/_save_file.py
@@ -5,6 +5,11 @@ import dataclasses_struct as dcs
 from mrds_util.raw._monster import Monster
 from mrds_util.raw._util import BinaryReadWriteable
 
+# Note: There appears to be 2 copies of save data in the save file and they appear to checked
+# against a checksum on start. Haven't fully tested the game's behavior, but it seems to load/use
+# use whichever one passes the checksum and if both fail the checksum then it resets the save data
+# (need to fully confirm this).
+
 
 @dcs.dataclass_struct(size="std", byteorder="little")
 class SaveFile(BinaryReadWriteable):
