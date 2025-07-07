abaac5ffe56b7ae8240d191d22e49212defeafe4
Christopher Wells <cwellsny@nycap.rr.com>
2025-07-06 18:41:18 -0700

    Fix player and ranch name lengths

--- mrds_util/raw/_save_file.py
+++ mrds_util/raw/_save_file.py
@@ -14,7 +14,8 @@ from mrds_util.raw._util import BinaryReadWriteable
 @dcs.dataclass_struct(size="std", byteorder="little")
 class SaveFile(BinaryReadWriteable):
     header: Annotated[bytes, 44]  # Game name twice?
-    ranch_name: Annotated[bytes, 17]
-    player_name: Annotated[bytes, 17]
-    unknown_a: Annotated[bytes, 412]
+    ranch_name: Annotated[bytes, 13]
+    unknown_a: Annotated[bytes, 4]
+    player_name: Annotated[bytes, 13]
+    unknown_b: Annotated[bytes, 416]
     monsters: Annotated[list[Monster], 2]  # TODO: find out correct number
