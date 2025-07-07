46ce806af101eff8b6b158567fd4f19645bde6ab
Christopher Wells <cwellsny@nycap.rr.com>
2025-07-06 17:42:49 -0700

    Extract more Monster struct fields

--- mrds_util/raw/_monster.py
+++ mrds_util/raw/_monster.py
@@ -7,5 +7,12 @@ from mrds_util.raw._util import BinaryReadWriteable
 
 @dcs.dataclass_struct(size="std", byteorder="little")
 class Monster(BinaryReadWriteable):
-    name: Annotated[bytes, 10]  # TODO: figure out correct number
-    unknown: Annotated[bytes, 410]
+    name: Annotated[bytes, 12]  # TODO: figure out correct number
+    unknown_a: Annotated[bytes, 28]
+    power: dcs.U16
+    intelligence: dcs.U16
+    skill: dcs.U16
+    speed: dcs.U16
+    defense: dcs.U16
+    life: dcs.U16
+    unknown: Annotated[bytes, 368]
