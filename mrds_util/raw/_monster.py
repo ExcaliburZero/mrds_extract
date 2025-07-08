c08b003d6553e7928fd03325c02c57a8103f0f88
Christopher Wells <cwellsny@nycap.rr.com>
2025-07-07 21:49:33 -0700

    Find monster species id location

--- mrds_util/raw/_monster.py
+++ mrds_util/raw/_monster.py
@@ -8,11 +8,13 @@ from mrds_util.raw._util import BinaryReadWriteable
 @dcs.dataclass_struct(size="std", byteorder="little")
 class Monster(BinaryReadWriteable):
     name: Annotated[bytes, 12]  # TODO: figure out correct number
-    unknown_a: Annotated[bytes, 28]
+    unknown_a: Annotated[bytes, 6]
+    species_id: dcs.U16
+    unknown_b: Annotated[bytes, 20]
     power: dcs.U16
     intelligence: dcs.U16
     skill: dcs.U16
     speed: dcs.U16
     defense: dcs.U16
     life: dcs.U16
-    unknown: Annotated[bytes, 368]
+    unknown_c: Annotated[bytes, 368]
